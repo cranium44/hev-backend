cd1ce2ea1eddd6b8f7dd2dd34cd1cab3e8487c74
Adabe Echaim <adabeechaim@gmail.com>
2021-04-03 21:59:36 +0100

    code refactor

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -2,35 +2,32 @@ const mongoose = require("mongoose");
 const uuid = require("uuid");
 const Item = require("../models/itemSchema");
 const url =
-  "mongodb+srv://hev:hevpassword@cluster0.iszw0.mongodb.net/hevdb?retryWrites=true&w=majority";
+    "mongodb+srv://hev:hevpassword@cluster0.iszw0.mongodb.net/hevdb?retryWrites=true&w=majority";
 
 mongoose
-
-  .connect(url, { useNewUrlParser: true }, { useUnifiedTopology: true })
-  .then(() => {
-    console.log("connected");
-  })
-  .catch((err) => {
-    console.log("Error");
-  });
+    .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const createItem = async (req, res) => {
-  const createdItem = new Item({
-    name: req.body.name,
-    price: req.body.price,
-    brand: req.body.brand,
-    pkgSize: req.body.pkgSize,
-    id: uuid(),
-  });
-  const result = await createdItem.save();
-
-  res.json(result);
+    const createdItem = new Item({
+        name: req.body.name,
+        categoryId: req.body.categoryId,
+        description: req.body.description,
+        id: uuid(),
+    });
+    const result = await createdItem.save();
+    res.json(result);
 };
 
 const getItems = async (req, res) => {
-  const items = await Item.find().exec();
-  res.json(items);
-}
+    const items = await Item.find().exec();
+    res.json(items);
+};
 
 exports.createItem = createItem;
 exports.getItems = getItems;
